a557c024bfdc12566dfcaa68486a9077f2990676
Renan Lira <renanlira@bluems.com>
2024-09-08 20:01:57 -0300

    Update .gitignore and add variables.tf

---
+++ variables.tf
@@ -0,0 +1,43 @@
+#### ECS APP CONFIGURATION ####
+
+variable "service_name" {
+  description = "The name of the ECS service"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+}
+
+variable "service_port" {
+  description = "The port on which the service listens"
+  type        = number
+}
+
+variable "service_cpu" {
+  description = "The number of CPU units to reserve for the service"
+  type        = number
+}
+
+variable "service_memory" {
+  description = "The amount of memory to reserve for the service"
+  type        = number
+}
+
+variable "service_listener" {
+  description = "The listener rule for the service"
+  type        = map(string)
+}
+
+#### NETWORK CONFIGURATION ####
+
+variable "vpc_id" {
+  description = "The SSM parameter name for the VPC ID"
+  type        = string
+}
+
+variable "private_subnets" {
+  description = "The SSM parameter name for the private subnet 1"
+  type        = string
+}
