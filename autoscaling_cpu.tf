662fa5e26727ce357145988f8effbeb808ebc386
Renan Lira <renanlira@bluems.com>
2024-09-22 16:36:15 -0300

    chore: elastic container service - cloudwatch scale-in

--- autoscaling_cpu.tf
+++ autoscaling_cpu.tf
@@ -46,3 +46,60 @@ resource "aws_cloudwatch_metric_alarm" "ecs_cpu_scale_out_alarm" {
 
   alarm_actions = [aws_appautoscaling_policy.ecs_cpu_scale_out[count.index].arn]
 }
+
+resource "aws_appautoscaling_policy" "ecs_cpu_scale_in" {
+  count = var.scale_type == "CPU" ? 1 : 0
+  name  = "${var.cluster_name}--${var.service_name}--${local.cpu_appautoscaling_resource_name}--scale-in-policy"
+
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+
+  policy_type = "StepScaling"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = var.scale_in_cooldown
+    metric_aggregation_type = var.scale_in_statistic
+
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = var.scale_in_adjustment
+    }
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      metric_interval_upper_bound = var.scale_in_cpu_threshold
+      scaling_adjustment          = var.scale_in_adjustment
+    }
+
+    step_adjustment {
+      metric_interval_lower_bound = var.scale_in_cpu_threshold
+      scaling_adjustment          = 0
+    }
+  }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "ecs_cpu_scale_in_alarm" {
+  count = var.scale_type == "CPU" ? 1 : 0
+
+  alarm_name        = "${var.cluster_name}--${var.service_name}--${local.cpu_appautoscaling_resource_name}--scale-in-alarm"
+  alarm_description = "Scale in when CPU exceeds ${var.scale_in_cpu_threshold}%"
+
+  comparison_operator = var.scale_in_comparison_operator
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  statistic           = var.scale_in_statistic
+
+  period             = var.scale_in_period
+  evaluation_periods = var.scale_in_evaluation_periods
+  threshold          = var.scale_in_cpu_threshold
+
+  dimensions = {
+    ClusterName = var.cluster_name
+    ServiceName = var.service_name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.ecs_cpu_scale_in[count.index].arn]
+}
